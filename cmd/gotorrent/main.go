09c16c4426dc7cf4ddd6bb55c7c7ae0531fdabbd
WendelHime <wendelhime@hotmail.com>
2024-04-20 17:16:47 -0300

    feat: add gotorrent cli

---
+++ cmd/gotorrent/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"log/slog"
+	"os"
+
+	"github.com/WendelHime/gotorrent/internal/decoder"
+	"github.com/WendelHime/gotorrent/internal/logic"
+)
+
+func main() {
+	var torrentPath string
+	var outputDir string
+	flag.StringVar(&torrentPath, "torrent", "~/Downloads/debian-12.5.0-amd64-netinst.iso.torrent", "Specify the input torrent file")
+	flag.StringVar(&outputDir, "output", "~/Downloads", "Specify the output directory")
+	flag.Parse()
+	f, err := os.Open(torrentPath)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelError}))
+
+	downloader := logic.NewDownloader(decoder.NewDecoder(), logger)
+	err = downloader.Download(f, outputDir)
+	if err != nil {
+		logger.Error("failed to download torrent", slog.Any("error", err))
+		return
+	}
+}
